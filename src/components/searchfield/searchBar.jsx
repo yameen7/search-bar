3136d2faae98f59587ef6c7619fe0f1820807b2d
Yameen Machhar <yameen.machhar@krishtechnolabs.com>
2023-11-02 16:58:50 +0530

    debounce added

--- src/components/searchfield/searchBar.jsx
+++ src/components/searchfield/searchBar.jsx
@@ -1,36 +1,40 @@
 import { useLazyQuery } from '@apollo/client';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { SEARCH_USER } from '../../users.gql';
 import styles from './SearchBar.module.css';
 
 const SearchBar = ({ setuser, inital, users }) => {
+  const [query, setQuery] = useState('');
 
   // Define the useLazyQuery hook and provide a callback function to handle the data
   const [executeQuery, { data }] = useLazyQuery(SEARCH_USER,{
-    onCompleted:()=>{
-      setuser(data?.searchUsers);
-      console.log("da",data);
-    }
+    onCompleted:()=>setuser(data?.searchUsers)
   });
 
-  const handleSearch = async (e) => {
-
-    // Call the executeQuery function, which will trigger the GraphQL query
-    if(e.target.value.length===0){
-      inital();
-    }
-    else{
-      setTimeout(async() => {
+  useEffect(() => {
+    // This useEffect will run whenever 'query' changes
+    const debounceTimer = setTimeout(async () => {
+      if (query.length === 0) {
+        inital();
+      } else {
         await executeQuery({
           variables: {
-            search: e.target.value,
-          }
+            search: query,
+          },
         });
-      }, 2000);
+      }
       
-    }
-   
-    
+    }, 1000); // Adjust the debounce delay as needed
+
+    return () => {
+      // Clean up the previous timer when 'query' changes again
+      clearTimeout(debounceTimer);
+      
+    };
+  }, [query, inital, executeQuery]);
+
+  const handleSearch = (e) => {
+    setQuery(e.target.value);
   };
 
   return (
@@ -38,10 +42,9 @@ const SearchBar = ({ setuser, inital, users }) => {
       <input
         type="text"
         placeholder="Search for products..."
-        // value={query}
-        onChange={(e) => handleSearch(e)}
+        value={query}
+        onChange={handleSearch}
       />
-      {/* <button onClick={handleSearch}>Search</button> */}
     </div>
   );
 };
